7d1ae6ef01b25f26f4dbc521310dbee3f8e6c7a1
Morgan Galvin <morgangalvin@yahoo.com>
2018-12-21 21:34:58 +1300

    added a navba, init components, structured

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Nav, Navbar, NavItem, MenuItem, NavProps, NavbarProps, NavItemProps, NavbarBrand, NavbarBrandProps } from 'react-bootstrap';
 import './App.css';
+
+function handleSelect(selectedKey) {
+  alert(`selected ${selectedKey}`);
+}
+
 
 class App extends Component{
   constructor(){
@@ -13,7 +18,6 @@ class App extends Component{
         <Body />
         <Footer />
       </div>
-      
     )
   }
 }
@@ -24,36 +28,92 @@ class Header extends Component{
   }
   render(){
     return(
-      <div className="navbar">
-      <div className="navbrand">  
-        <img src="https://cdn2.iconfinder.com/data/icons/black-animal-svg-icons/512/pig_piggy_bank_money_finance-128.png" alt="pig-icon" />
-        <span><b>LtP</b></span>
-      </div>
-      <div className="link-ctr">
-          <span id='home' className="highlight">Home</span>
-          <span id='code'>Code</span>
-          <span id='photo'>Photo</span>
-          <span id='contact'>Contact</span>
-      </div>
-    </div>
+      <Nav bsStyle="pills" activeKey={1} onSelect={handleSelect}>
+      <NavItem eventKey={1} href="/home">
+      Home
+      </NavItem>
+      <NavItem eventKey={2} title="Item">
+        Technologies
+      </NavItem>
+      <NavItem eventKey={3}>
+        Projects
+      </NavItem>
+      <NavItem eventKey={3}>
+        Contact
+      </NavItem>
+    </Nav>
     )
   }
 }
 
-
 class Body extends Component{
-  constructor(){
-    super();
-  }
   render(){
     return(
       <div>
+      <Home />
+      <Tech />
+      <Projects />
+      <Contact />
+      </div>
+    )
+  }
+}
+
+class Home extends Component{
+  render(){
+    return(
+      <div id="home" className="body">
         <h2>Howdy, my name is Morgan Galvin.</h2>
         <p>I am a freelance web developer and photographer. I have just moved to Wellington, NZ, from the United States, and am seeking employment as a developer.
           <br />On this site, you will find references to my work, projects, and expertise.
         </p>
       </div>
       
+    )
+  }
+}
+
+class Tech extends Component{
+  render(){
+    return(
+      <div id="tech" className="body">
+        <h1>Hey, I know how a lot about Javascript, React, Redux, d3.js, and, of course, HTML/CSS/SCSS</h1>
+        <h2>Hire me!!!</h2>
+        <p>this is a placeholderrrrr</p>
+      </div>
+    )
+  }
+}
+
+class Projects extends Component{
+  render(){
+    return(
+      <div id="projects" className="body">
+        <h1>Yeah, I've done some pretty cool stuff</h1>
+        <h2>Here is a list!</h2>
+        <ul>
+          <li>Markdown Previewer!</li>
+          <li>Drum Machine!</li>
+          <li>Klackers!</li>
+        </ul>
+        <p>Check out my Github and Codepen for more!</p>
+      </div>
+    )
+  }
+}
+
+class Contact extends Component{
+  render(){
+    return(
+      <div id="contact" className="body">
+        <h1>Let's get in touch</h1>
+        <h3>I am here to learn and help you build the kick ass platform that your company needs!</h3>
+        <p>morgan@ludwigthepig.com
+          <br />
+          +64 21 029 81349 <i>(in New Zealand temporarily)</i>
+        </p>
+        
+      </div>
     )
   }
 }
