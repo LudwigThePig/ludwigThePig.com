7064373131678c43616623c06318e55c9561efd2
Morgan Galvin <morgangalvin@yahoo.com>
2019-01-06 21:38:48 +1300

    styled about section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.scss';
+import styles from './App.scss';
+import classNames from 'classnames';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -155,7 +156,7 @@ class Home extends Component{
           <h2>Howdy, my name is </h2>
           <h1>Morgan Galvin</h1>
           <h2 style={{textAlign: 'center'}}>Javascript Extraordinaire</h2> 
-          <p>I am a web developer living Wellington, NZ. On this site, you will find references to my work, portfolio, and expertise. I am seeking new opportunities.
+          <p>I am a fullstack web developer living Wellington, NZ. On this site, you will find references to my work, portfolio, and expertise. I am seeking new opportunities.
           </p>
           <h4><a href='https://www.duracellenergybank.com/'>INSP</a></h4>
         </div>
@@ -331,18 +332,57 @@ class Portfolio extends Component{
   }
 }
 class About extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      more: false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState({
+      more: !this.state.more
+    })
+  }
+ 
+
   render(){
-    return(
-      <div id="about" className="body">
+    if(!this.state.more){
+      return(
+      <div id="about">
         <h2>About Morgan</h2>
-        <p>My name is Morgan. I moved to Colorado from New York in Janurary 2012. In December 2018, I will be taking a one way flight to Wellington, New Zealand. When I'm not building web apps or taking photos, I am either running, skiing, biking, or running some more. The outdoors is what pulled me out to Colorado. 
-        <br />
-        I received my Bachelors of Arts from Fort Lewis College in Durango, studying philosophy. While I do retain interest, philosophy has never interested me as a career path for me. Although, I do find the critical and analytical skills that I have developed from studying philosophy have helped me flourish in other fields, such as programming.</p>
+        <p>
+            In 2012, I began studying philosophy and knew that I would not pursue it professionally. I knew that the critical and analytical skills that I would develop as a philosopher would guide me in whatever profession I decided to pursue.
+            </p>
+        <span id='more' onClick={this.handleClick}>See More</span>            
+
+        <h2>About Ludwig</h2>
+        <p>The name Ludwig The Pig stems from one of favorite philosophers, Ludwig Wittegenstein. Ludwig just so happens to rhyme with pig. Hence, the name 'Ludwig the Pig'. Ludwig has developed his own persona and the curious visitor may catch a rare glance of him.</p>
+      </div>
+    ) 
+    } else {
+      const longClasses = classNames('body', 'long');
+    return(
+      <div id="about" className={longClasses}>
+        <h2>About Morgan</h2>
+        <p>
+            In 2012, I began studying philosophy and knew that I would not pursue it professionally. I knew that the critical and analytical skills that I would develop as a philosopher would guide me in whatever profession I decided to pursue.
+            <br /><br />
+            Fast forward to a February of 2018, I was building a website for my photography and I couldn't find the right template to use on Wix. So, I asked myself, how can I build this myself? That question rippled into questions about JavaScript, frameworks, databases, and internet protocols. It was clear that this was the line of work that I wanted to pursue.
+            <br /><br />
+            The challenges in programming and the challenge of learning how to program are similar to the problems in philosophy. That is what keeps me engaged and relevant. There is a lot for me to learn in tech, but there will always be more to learn and that is what I love about technology. It is a fast paced field that rewards people such as myself, who are quick and eager to learn.
+            <br /><br />
+            When I'm not coding away, I like to go for long runs, ride my bike, ski, or do anything outdoors. I race at the elite level in trail running. The process of training and competing is fun for me, but it has taught me a lot about persistence and being goal oriented.
+          </p>
+          <span id='more' onClick={this.handleClick}>See Less</span>            
+
 
         <h2>About Ludwig</h2>
         <p>The name Ludwig The Pig stems from one of favorite philosophers, Ludwig Wittegenstein. Ludwig just so happens to rhyme with pig. Hence, the name 'Ludwig the Pig'. Ludwig has developed his own persona and the curious visitor may catch a rare glance of him.</p>
       </div>
     )
+    }
   }
 }
 class Contact extends Component{
