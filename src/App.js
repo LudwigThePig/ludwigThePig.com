9b1a37638f618a2f1dcfdff9193e3599c4c45a3e
Morgan Galvin <morgan@ludwigthepig.com>
2019-06-03 16:58:42 -0600

    adjusted h1 styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar.js';
 import Body from './components/Body.js';
+import Footer from './components/Footer.js';
 
 import styles from './App.scss';
 import classNames from 'classnames';
@@ -24,28 +25,4 @@ class App extends Component{
   }
 }
 
-class Footer extends Component{
-  render(){
-    return(
-      <footer id='footer'>		
-      <a href="https://www.linkedin.com/in/morgangalvin">
-        <FontAwesomeIcon icon={['fab', 'linkedin']} className='fa' size="3x" />
-      </a>
-      <a href="https://github.com/LudwigThePig">
-        <FontAwesomeIcon icon={['fab', 'github']} className='fa' size="3x" />
-      </a>
-      <a href="https://codepen.io/moegain/">
-        <FontAwesomeIcon icon={['fab', 'codepen']} className='fa'  size="3x" />
-      </a>
-      <a href="https://www.instagram.com/morgangalvinphoto/">
-      <FontAwesomeIcon icon={['fab', 'instagram']} className='fa' size="3x" />
-      </a>
-      <a href="https://www.strava.com/athletes/5897641">
-      <FontAwesomeIcon icon={['fab', 'strava']} className='fa' size="3x" />
-      </a>
-      <br /> <br />
-      </footer>
-    )
-  }
-}
 export default App;
